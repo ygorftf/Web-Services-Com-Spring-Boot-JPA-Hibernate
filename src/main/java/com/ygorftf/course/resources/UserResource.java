9259e202db3575dfb94147178cb44321b4a345cf
Ygor Torres <ygorftf@gmail.com>
2023-10-27 06:29:11 -0300

    Service layer, component registration

--- src/main/java/com/ygorftf/course/resources/UserResource.java
+++ src/main/java/com/ygorftf/course/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.ygorftf.course.resources;
 
 import com.ygorftf.course.entities.User;
+import com.ygorftf.course.servicies.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User test = new User(1L,"ygor","ygorftf@gmail.com","999999999","1234569");
-        return ResponseEntity.ok(test);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
