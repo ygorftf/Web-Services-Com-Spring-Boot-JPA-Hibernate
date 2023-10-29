b55b0120e68805b1d9badc9f6c9788121974c284
Ygor Torres <ygorftf@gmail.com>
2023-10-28 21:15:57 -0300

    OrderStatus enum

--- src/main/java/com/ygorftf/course/entities/Order.java
+++ src/main/java/com/ygorftf/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.ygorftf.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ygorftf.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,11 +24,14 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -45,6 +49,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
