8940ef9a432d236a865389efc8279295780f9455
Ygor Torres <ygorftf@gmail.com>
2023-10-29 07:43:29 -0300

    Product

--- src/main/java/com/ygorftf/course/entities/Category.java
+++ src/main/java/com/ygorftf/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.ygorftf.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -14,6 +16,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {}
     public Category(Integer id, String name) {
@@ -35,6 +40,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/ygorftf/course/servicies/ProductService.java
@@ -0,0 +1,25 @@
+package com.ygorftf.course.servicies;
+
+import com.ygorftf.course.entities.Product;
+import com.ygorftf.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
