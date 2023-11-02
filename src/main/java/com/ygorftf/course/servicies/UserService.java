53537ff801af1e63255a10c4ad653e9a851e784c
Ygor Torres <ygorftf@gmail.com>
2023-11-02 09:56:44 -0300

    Exception handling - update

--- src/main/java/com/ygorftf/course/servicies/UserService.java
+++ src/main/java/com/ygorftf/course/servicies/UserService.java
@@ -4,6 +4,7 @@ import com.ygorftf.course.entities.User;
 import com.ygorftf.course.repositories.UserRepository;
 import com.ygorftf.course.servicies.exceptions.DatabaseException;
 import com.ygorftf.course.servicies.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
